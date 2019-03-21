c6f5b9ae0ead887a3e82015331e5e71842b7adbe
alphaboss16 <34588457+alphaboss16@users.noreply.github.com>
2019-03-20 21:37:51 -0400

    added skeleton code to test api

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import praw
-life = praw.Reddit()
+
+reddit = praw.Reddit('bot1', user_agent='bot1 user agent')
+subreddit = reddit.subreddit('FortniteCompetitive+FortniteBR')
+
+for submission in subreddit.stream.comments(skip_existing=True):
+    print(submission.body)
